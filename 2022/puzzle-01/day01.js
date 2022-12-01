6cdf8028b9187c5772f59552f4b65d69d0649342
Alberto Fortes Sanchez <albertofortes@MacBook-Pro-de-Alberto.local>
2022-12-01 13:24:32 +0100

    AOC 2022 - day 1

---
+++ 2022/puzzle-01/day01.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+
+let i = 0
+
+const aCalories = fs.readFileSync('input.txt', {encoding: 'utf8'})
+  .split('\n')
+  .map(calories => parseInt(calories))
+  .reduce((accumulator, calories) => {
+    if(isNaN(calories)) {
+      i++
+      accumulator[i] = 0
+      return accumulator
+    }
+
+    accumulator[i] += calories
+    return accumulator
+  }, [0])
+
+  const maxCalories = Math.max(...aCalories)
+
+  const topThreeElvesCalories = aCalories
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((accumulator, calories) => accumulator + calories, 0)
+
+console.log(aCalories.sort((a, b) => b - a))
+console.log(`Part 1. The Elf carrying the most Calories, is carrying: ${maxCalories} calories`)
+console.log(`Part 2. The top 3 elves are carrying: ${topThreeElvesCalories} calories`)
