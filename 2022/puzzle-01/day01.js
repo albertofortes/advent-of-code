39ff55117b68106235df0785c3aa554715b9503c
Alberto Fortes Sanchez <albertofortes@MacBook-Pro-de-Alberto.local>
2022-12-01 13:38:20 +0100

    day 1 fix indentation

--- 2022/puzzle-01/day01.js
+++ 2022/puzzle-01/day01.js
@@ -16,13 +16,13 @@ const aCalories = fs.readFileSync('input.txt', {encoding: 'utf8'})
     return accumulator
   }, [0])
 
-  const maxCalories = Math.max(...aCalories)
+const maxCalories = Math.max(...aCalories)
 
-  const topThreeElvesCalories = aCalories
-    .sort((a, b) => b - a)
-    .slice(0, 3)
-    .reduce((accumulator, calories) => accumulator + calories, 0)
+const topThreeElvesCalories = aCalories
+  .sort((a, b) => b - a)
+  .slice(0, 3)
+  .reduce((accumulator, calories) => accumulator + calories, 0)
 
-console.log(aCalories.sort((a, b) => b - a))
+//console.log(aCalories.sort((a, b) => b - a))
 console.log(`Part 1. The Elf carrying the most Calories, is carrying: ${maxCalories} calories`)
 console.log(`Part 2. The top 3 elves are carrying: ${topThreeElvesCalories} calories`)
