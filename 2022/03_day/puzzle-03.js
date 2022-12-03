143bb4958059933e62d2dec71c7caf563e02bb38
Alberto Fortes Sanchez <albertofortes@MacBook-Pro-de-Alberto.local>
2022-12-03 12:19:31 +0100

    Day 3 part 1

---
+++ 2022/03_day/puzzle-03.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+const data = fs.readFileSync(path.join(__dirname, 'input.txt'), {encoding: 'utf8'})
+  .split('\n')
+  
+const getPriority = letter => alphabet.indexOf(String(letter)) + 1
+
+const getItemPosition = (item, rucksack) => rucksack.indexOf(String(item))
+
+const findDuplicateItemsByCompartment = rucksack => {
+  const items = Array.from(rucksack)
+  const compartmentOne = items.slice(0, items.length / 2)
+  const compartmentTwo = items.slice(items.length / 2)
+  
+  return items.filter((item, index) => compartmentOne.indexOf(item) !== -1 && compartmentTwo.indexOf(item) !== -1)[0]
+}
+
+const sumOfAllDuplicatedItems = rucksacks => rucksacks.reduce((accum, rucksack) => accum + getPriority(findDuplicateItemsByCompartment(rucksack)), 0)
+
+module.exports = { alphabet, getPriority, getItemPosition, findDuplicateItemsByCompartment, sumOfAllDuplicatedItems }
+
+
+console.log(data)
+console.log(`Part 1. The sum of the priorities of those item types duplicated is: ${sumOfAllDuplicatedItems(data)}`)
---
+++ 2022/03_day/puzzle-03.test.js
@@ -0,0 +1,42 @@
+const { alphabet, getPriority, getItemPosition, findDuplicateItemsByCompartment, sumOfAllDuplicatedItems } = require("./puzzle-03")
+
+const testData = [
+  'vJrwpWtwJgWrhcsFMMfFFhFp',
+  'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+  'PmmdzqPrVvPwwTWBwg',
+  'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+  'ttgJtRGJQctTZtZT',
+  'CrZsJsPPZsGzwwsLwLmpwMDw'
+]
+
+describe('Day 3', () => {
+  it('Get position of letter in a row', () => {
+    expect(getItemPosition('v', testData[0])).toBe(0)
+    expect(getItemPosition('Q', testData[4])).toBe(8)
+  })
+
+  it('Get priorities of each item', () => {
+    expect(getPriority('a')).toBe(1)
+    expect(getPriority('Z')).toBe(52)
+    expect(getPriority('L')).toBe(38)
+    expect(getPriority('v')).toBe(22)
+  })
+
+  it('Find duplicate items by rucksack', () => {
+    expect(findDuplicateItemsByCompartment(testData[0])).toBe('p')
+    expect(findDuplicateItemsByCompartment(testData[1])).toBe('L')
+  })
+
+  it('Each rucksack duplicated item priority are', () => {
+    expect(getPriority(findDuplicateItemsByCompartment(testData[0]))).toBe(16)
+    expect(getPriority(findDuplicateItemsByCompartment(testData[1]))).toBe(38)
+    expect(getPriority(findDuplicateItemsByCompartment(testData[2]))).toBe(42)
+    expect(getPriority(findDuplicateItemsByCompartment(testData[3]))).toBe(22)
+    expect(getPriority(findDuplicateItemsByCompartment(testData[4]))).toBe(20)
+    expect(getPriority(findDuplicateItemsByCompartment(testData[5]))).toBe(19)
+  })
+
+  it('And the priorities of those item types is', () => {
+    expect(sumOfAllDuplicatedItems(testData)).toBe(157)
+  })
+})
