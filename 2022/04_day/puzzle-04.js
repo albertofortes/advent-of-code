b760485b10397249252e9776548fcef538d4f8c7
Alberto Fortes Sanchez <albertofortes@MacBook-Pro-de-Alberto.local>
2022-12-04 19:48:36 +0100

    day small refactor

--- 2022/04_day/puzzle-04.js
+++ 2022/04_day/puzzle-04.js
@@ -10,26 +10,30 @@ const createPairs = data => {
 
 const pairs = createPairs(data)
 
-// To refactor duplicate code
-const isFullyOverlapping = pair => {
+const getRanges = pair => {
   const [elfOneMin, elfOneMax] = pair[0].split("-").map(Number)
   const [elfTwoMin, elfTwoMax] = pair[1].split("-").map(Number)
-  
+
+  return {
+    elfOneMin, elfOneMax, elfTwoMin, elfTwoMax
+  }
+}
+
+// To refactor duplicate code
+const isFullyOverlapping = pair => {
+  const { elfOneMin, elfOneMax, elfTwoMin, elfTwoMax } = getRanges(pair)
   return (elfOneMin >= elfTwoMin && elfOneMax <= elfTwoMax) || (elfOneMin <= elfTwoMin && elfOneMax >= elfTwoMax) ? 1 : 0
 }
 
 // To refactor duplicate code
 const isPartiallyOverlapping = pair => {
-  const [elfOneMin, elfOneMax] = pair[0].split("-").map(Number)
-  const [elfTwoMin, elfTwoMax] = pair[1].split("-").map(Number)
-  
+  const { elfOneMin, elfOneMax, elfTwoMin, elfTwoMax } = getRanges(pair)
   return (elfOneMin <= elfTwoMax && elfOneMax >= elfTwoMin) ? 1 : 0
 }
 
 const fullyContainedTotal = pairs => pairs.reduce((accum, pair) =>  accum + isFullyOverlapping(pair), 0)
 
 const partiallyOverlappingTotal = pairs => pairs.reduce((accum, pair) =>  accum + isPartiallyOverlapping(pair), 0)
-
 
 module.exports = { createPairs, fullyContainedTotal, partiallyOverlappingTotal }
 
